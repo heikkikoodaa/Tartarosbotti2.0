0f3ccfe9dc5a2c2c87156ce1160e7875523d5c12
Heikki Riihinen <heikki.riihinen@avanio.com>
2023-05-15 13:46:04 +0300

    Update to follow config files and add presence check

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-const { Client, GatewayIntentBits } = require('discord.js')
-require('dotenv').config()
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
-
-const BOT_TOKEN = process.env.BOT_TOKEN
+const { client } = require('./configs/bot_config')
+const { BOT_TOKEN } = require('./configs/constants')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('presenceUpdate', (oldPresence, newPresence) => {
+  // Ignore if user is bot or previous presence was offline
+  if (newPresence.user.bot || oldPresence.status === 'offline') return
+
+  // If user's new presence is streaming, send a notification to the channel with the user's stream URL
+  if (newPresence.activities[0].type === 'STREAMING') {
+    newPresence.guild.channels.cache
+      .find((channel) => channel.id === '1105870680081309799')
+      .send(
+        `${newPresence.user} is now streaming at ${newPresence.activities[0].url}`,
+      )
+  }
 })
 
 client.login(BOT_TOKEN)
