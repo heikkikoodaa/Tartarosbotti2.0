795f2b2e009f3d7133a2d9aca841420a84276ef4
Heikki Riihinen <heikki.riihinen@avanio.com>
2023-05-15 17:15:55 +0300

    Export mongo_url

--- configs/constants.js
+++ configs/constants.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 // Get the bot token from the .env file
 const BOT_TOKEN = process.env.BOT_TOKEN
+const MONGO_URL = process.env.MONGO_URL
 
 // Export the bot token
 module.exports = {
   BOT_TOKEN,
+  MONGO_URL,
 }
