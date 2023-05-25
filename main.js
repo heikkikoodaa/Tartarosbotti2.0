8dd83892d2645e413d1d7cdc50b5091702e95cd7
Heikki Riihinen <heikki.riihinen@avanio.com>
2023-05-25 16:05:14 +0300

    Add logic to check stream status

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder } = require('discord.js')
 const { client } = require('./configs/bot_config')
 const { BOT_TOKEN, BACKEND_URL } = require('./configs/constants')
 const { startApp } = require('./app/server')
@@ -7,17 +8,118 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('presenceUpdate', (oldPresence, newPresence) => {
-  // Ignore if user is bot or previous presence was offline
-  if (newPresence.user.bot || oldPresence.status === 'offline') return
+const createUser = async (user) => {
+  const newUser = {
+    discordId: user.id,
+    username: user.username,
+    twitchUrl: user.twitchUrl,
+    avatar: user.avatar,
+  }
 
-  // If user's new presence is streaming, send a notification to the channel with the user's stream URL
-  if (newPresence.activities[0].type === 'STREAMING') {
-    newPresence.guild.channels.cache
-      .find((channel) => channel.id === '1105870680081309799')
-      .send(
-        `${newPresence.user} is now streaming at ${newPresence.activities[0].url}`,
+  try {
+    const { data } = await axios.post(`${BACKEND_URL}/users`, newUser)
+
+    if (!data.success) {
+      return false
+    }
+
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
+}
+
+const updateStreamStatus = async (user, isStreaming) => {
+  const updatedUser = {
+    isStreaming: isStreaming,
+  }
+
+  try {
+    const { data } = await axios.patch(
+      `${BACKEND_URL}/users/${user.discordId}`,
+      updatedUser,
+    )
+
+    if (!data.success) {
+      return false
+    }
+
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
+}
+
+const checkUser = async (user) => {
+  // Check if user exists in database
+  try {
+    const { data } = await axios.get(`${BACKEND_URL}/users/${user.id}`)
+
+    if (!data.success) {
+      // If user does not exist, create new user
+      const newUserCreated = createUser(user)
+
+      if (!newUserCreated) return
+      console.log(`New user created: ${user.username}`)
+      return data.user
+    }
+
+    // If user exists, return their data
+    return data.user
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+client.on('presenceUpdate', (oldPresence, newPresence) => {
+  // Ignore if presence update is not from a user
+  if (newPresence.user.bot) return
+  const user = newPresence.user
+
+  try {
+    if (!newPresence.activities.length) return
+
+    // Check if user starts streaming
+    const isStreaming = newPresence.activities.some((activity) => {
+      if (activity.type === 'STREAMING') {
+        user.twitchUrl = activity.url
+        return true
+      }
+    })
+
+    // Ignore if user is not streaming
+    if (!isStreaming) return
+
+    // Check if user exists in database and return their data
+    const fetchedUser = checkUser(user)
+
+    // Ignore if user is already streaming
+    if (fetchedUser.isStreaming) return
+
+    // Update user stream status
+    updateStreamStatus(fetchedUser, true)
+
+    // Check if user stops streaming
+    const streamEnding = oldPresence.activities.some((oldActivity) => {
+      const newPresenceIsNotStreaming = newPresence.activities.some(
+        (newActivity) => newActivity.type !== 'STREAMING',
       )
+
+      // If user's old presence is streaming and new presence is not streaming, user is ending stream
+      if (oldActivity.type === 'STREAMING' && newPresenceIsNotStreaming) {
+        return true
+      }
+      return false
+    })
+
+    if (streamEnding) {
+      // Update user stream status
+      updateStreamStatus(fetchedUser, false)
+    }
+  } catch (error) {
+    console.error(error)
   }
 })
 
@@ -27,19 +129,44 @@ client.on('messageCreate', async (msg) => {
 
   if (msg.content === '!test') {
     try {
-      const { data } = await axios.get(`${BACKEND_URL}/users`)
-      msg.reply({ content: data, ephemeral: true })
-    } catch (error) {
-      if (error.response.status === 404) {
-        msg.reply({ content: 'No users found', ephemeral: true })
-        return
-      } else {
-        msg.reply({
-          content: 'Something went wrong. Please try again later.',
-          ephemeral: true,
-        })
+      const { data } = await axios.get(`${BACKEND_URL}/users/${msg.author.id}`)
+      if (!data.success) {
+        msg.reply('User not found')
         return
       }
+      const avatarEmbed = new EmbedBuilder()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setImage(
+          `https://cdn.discordapp.com/avatars/${data.user.discordId}/${data.user.avatar}.webp?size=256`,
+        )
+        .setThumbnail(
+          `https://cdn.discordapp.com/avatars/${data.user.discordId}/${data.user.avatar}.webp`,
+        )
+      msg.channel.send({ content: 'User found', embeds: [avatarEmbed] })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  if (msg.content === '!start') {
+    try {
+      const updatedUser = {
+        isStreaming: true,
+      }
+      const { data } = await axios.patch(
+        `${BACKEND_URL}/users/${msg.author.id}`,
+        updatedUser,
+      )
+
+      if (!data.success) {
+        msg.reply('Stream status could not be updated')
+        return
+      }
+
+      msg.reply(`Stream status updated for ${msg.author.username}`)
+    } catch (error) {
+      console.error(error)
     }
   }
 })
