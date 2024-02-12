2e843a128f091bc2dc874a3e0ced03905f12dcb5
Hexutin <32955793+torastrengel@users.noreply.github.com>
2024-02-12 15:07:04 +0200

    Implement IGDB connection

--- events/presence.js
+++ events/presence.js
@@ -1,9 +1,15 @@
 const axios = require('axios')
 const TartarosError = require('../classes/TartarosError')
-const { BACKEND_URL, STREAM_NOTIFICATION_CHANNEL } = require('../configs/constants')
+const {
+  BACKEND_URL,
+  STREAM_NOTIFICATION_CHANNEL,
+  TWITCH_CLIENT_ID,
+} = require('../configs/constants')
 const { checkUser } = require('../utils/userFunctions')
 const { client } = require('../configs/bot_config')
-const { ActivityType } = require('discord.js')
+// eslint-disable-next-line no-unused-vars
+const { ActivityType, EmbedBuilder, User } = require('discord.js')
+const { getTokenFromDB } = require('../configs/token_config')
 
 const areNotificationsEnabled = process.env.NOTIFICATIONS_ENABLED === 'true'
 
@@ -14,7 +20,10 @@ const updateStreamStatus = async (user, isStreaming) => {
   }
 
   try {
-    const { data } = await axios.patch(`${BACKEND_URL}/users/${user.discordId}`, newStreamStatus)
+    const { data } = await axios.patch(
+      `${BACKEND_URL}/users/${user.discordId}`,
+      newStreamStatus,
+    )
 
     if (!data.success) {
       throw new TartarosError(data.message, 'Patching user information')
@@ -35,14 +44,66 @@ const updateStreamStatus = async (user, isStreaming) => {
   }
 }
 
-const announceStream = (user) => {
+const getGameInfo = async (game) => {
+  try {
+    const baseUrl = 'https://api.igdb.com/v4'
+    const token = await getTokenFromDB()
+
+    const { data } = await axios.post(
+      `${baseUrl}/games`,
+      `search "${game}"; fields name, cover.url; where category = 0;`,
+      {
+        headers: {
+          Accept: 'application/json',
+          'Client-ID': TWITCH_CLIENT_ID,
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    if (data.length > 0) {
+      const gameData = data[0]
+
+      if (gameData.cover.url) {
+        const originalUrl = gameData.cover.url
+        let newUrl = originalUrl.startsWith('//') ? 'https:' + originalUrl : originalUrl
+        newUrl = newUrl.replace('t_thumb', 't_cover_big')
+
+        gameData.cover.url = newUrl
+      }
+
+      return gameData
+    } else {
+      console.log('No games found with that name')
+      return null
+    }
+  } catch (error) {
+    console.error(`[ERROR]: Error when fetching game data - ${error}`)
+    return null
+  }
+}
+
+const announceStream = async (user) => {
   const channel = client.channels.cache.get(STREAM_NOTIFICATION_CHANNEL)
+  const gameData = await getGameInfo(user.streamGame)
 
   if (channel) {
-    channel.send(`${user.username} aloitti striimin osoitteessa - ${user.twitchUrl}\n${user.streamHeading || ''}`)
-    console.log(`${user.username} aloitti striimin`)
+    const streamEmbed = new EmbedBuilder()
+      .setColor('#825791')
+      .setTitle(user.streamHeading || `Käyttäjän ${user.username} striimi alkaa`)
+      .setURL(user.twitchUrl)
+      .setAuthor({
+        name: user.username,
+        iconURL: user.avatar,
+      })
+      .addFields(
+          { name: 'Peli', value: user.streamGame || 'Tuntematon peli' },
+      )
+      .setImage(gameData.cover.url)
+
+    channel.send({ embeds: [streamEmbed] })
   } else {
-    console.error('Channel not found!')
+    console.error('Striimi-ilmoituskanavaa ei löytynyt!')
   }
 }
 
@@ -50,8 +111,14 @@ const handlePresence = async (oldPresence, newPresence) => {
   // Ignore if presence update is not from a user
   if (newPresence.user.bot) return
 
+  /**
+   * @type {User}
+   */
   const user = newPresence.user
+  const userAvatar = user.avatarURL()
   const fetchedUser = await checkUser(user)
+
+  fetchedUser.avatar = userAvatar
 
   let isStreamStarting = false
   let wasAlreadyStreaming = false
@@ -90,10 +157,12 @@ const handlePresence = async (oldPresence, newPresence) => {
     if (fetchedUser.isStreaming && !isStreamStarting) {
       await updateStreamStatus(fetchedUser, false)
     }
-
   } catch (error) {
     console.error(error.message || error)
   }
 }
 
-module.exports = handlePresence
+module.exports = {
+  getGameInfo,
+  handlePresence,
+}
